f8084323fd54296f3f983120fe722187b555d1c8
AbhishekASN <abhishekasn08@gmail.com>
2023-02-08 20:05:40 +0530

    Submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,7 @@
 package com.driver;
 
 public class Main {
+
     public static class A {
         public String meth() { return "Invoking method from class A"; }
     }
@@ -9,10 +10,10 @@ public class Main {
         @Override
         public String meth(){ return "Method is overridden in Extended class B"; }
   }
-  public static void main(String[] args){
+  public static void main(String[] args) {
+
         B b= new B();
         b.meth();
-
 
         B b1 = new B();
         b1.meth();
